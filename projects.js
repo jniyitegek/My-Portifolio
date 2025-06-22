3e9c02825414e95866d4523fc892cf006d3af470
Jean Philippe Niyitegeka <j.niyitegek@alustudent.com>
2025-06-22 12:25:27 +0200

    my portfolio

--- projects.js
+++ projects.js
@@ -6,31 +6,31 @@ const projectList = [
     title: "SOUL HOSTEL'S WEBSITE",
     description: "It offers accommodation for students around Kigali",
     picture: "./Images/soulhostels.png",
-    link:"google.com"
+    link:"https://www.soulhostels.rw/"
   },
   {
     title: "BOWSTRING'S WEBSITE",
     description: "It is online opportunities plaform for students and professionals",
     picture: "./Images/Aguka.png",
-    link:"google.com"
+    link:"https://bow-strings.vercel.app/"
   },
   {
     title: "GIFTFIT'S WEBSITE",
     description: "They collect idle clothes and distribute it to the poor",
     picture: "./Images/giftfit.png",
-    link:"google.com"
+    link:"https://giftfit.vercel.app/"
   },
   {
     title: "SOMA FOUNDATION RWANDA WEB",
     description: "They Sponsor vurnerable students to get education",
     picture: "./Images/somaf.png",
-    link:"google.com"
+    link:"https://sfrw.org/"
   },
   {
     title: "ADVANCED UNIQUE PORTFOLIO WEB",
     description: "Made with NEXT JS and showcases my creativity",
     picture: "./Images/myweb.png",
-    link:"google.com"
+    link:"https://jp-kappa.vercel.app/"
   }
 ];
 
@@ -41,7 +41,8 @@ const markup = projectList
       <img id="projectback" src="${item.picture}" alt="${item.title} image" />
       <h1>${item.title}</h1>
       <p>${item.description}</p>
-      <button>Visit</button>
+      <a href="${item.link}" target="_blank" rel="noopener noreferrer"><button>Visit</button></a>
+      
     </div>
   `)
   .join('');          
